2ce5683ffc915cb79d3140b77fcc5add2ba01bd9
Neil <zooming.in@hotmail.com>
2021-07-07 22:15:49 +1000

    updates

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+    new_string = ""
+        if array.size == 1
+            return array.join
+        elsif array.size == 2
+            return new_string += "#{array[0]} and #{array[1]}"
+        else
+            array.each{|item| new_string += item == array[-1]? "and #{item}" : "#{item}, "}
+            return new_string
+        end
 end
